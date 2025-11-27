44ea3bb5a098de8d26d2af34d7df83aaa7b933ba
M. Damian Mulligan <damian.mulligan@oregan.net>
2025-11-27 12:00:35 -0700

    Let LSP use paths.

--- lua/dmulligan/lazy/lsp.lua
+++ lua/dmulligan/lazy/lsp.lua
@@ -65,6 +65,9 @@ return {
                 },
             }, {
                 {
+                    name = "path",
+                },
+                {
                     name = "buffer",
                 },
             }),
